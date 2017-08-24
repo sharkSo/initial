954f2ba9d0061e0cff56ebec5d7740b65836fe11
Rodrigo <Rodrigo@DESKTOP-SGGK8MK>
2017-08-24 10:24:28 -0300

    auuuu

---
+++ TOTAL_LIKES_BY_POST/src/main/java/com/silvershark/control/AnalyticCtrl.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package com.silvershark.control;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import com.silvershark.connection.PhoenixExecuteSQL;
+import com.silvershark.connection.PhoenixJDBCConnection;
+
+/**
+ * @author Rodrigo
+ *
+ */
+public class AnalyticCtrl {
+	
+	private static final String TOTAL_LIKES_BY_DAY = "SELECT TO_DATE(DATE_INPUT), COUNT(*) FROM FACEBOOK_FEED_DATA_LIKES GROUP BY DATE_INPUT ORDER BY TO_DATE(DATE_INPUT) ASC";
+
+	@SuppressWarnings("static-access")
+	public List<String> getTotalLikesByDay(){
+		PhoenixJDBCConnection jdbc = new PhoenixJDBCConnection();
+		Connection conn = jdbc.createConnection();
+		PhoenixExecuteSQL sql = new PhoenixExecuteSQL();
+		ResultSet rs = sql.executeQUERY(TOTAL_LIKES_BY_DAY, conn);
+		try {
+			return sql.writeResultSet(rs);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
---
+++ TOTAL_SENTIMENT_BY_DAY/src/main/java/com/silvershark/view/AnalyticMB.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.silvershark.view;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ * @author Rodrigo
+ *
+ */
+@ManagedBean
+@ViewScoped
+public class AnalyticMB {
+
+	
+}
